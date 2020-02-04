bbadadd9fef95e65a792b5044c80649a3e1147eb
fierdy_pandu <lifeisubw@gmail.com>
2020-02-04 17:43:02 +0700

    add association has_many to person model

--- app/models/house.rb
+++ app/models/house.rb
@@ -4,4 +4,9 @@ class House < ApplicationRecord
     # Remember, has_many is just a method where the first argument is
     # the name of the association, and the second argument is an options
     # hash.
+    has_many(:residents, {
+      primary_key: :id,
+      foreign_key: :house_id,
+      class_name: :Person
+    })
 end
