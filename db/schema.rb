375f65ee2fd62a3ea02345ff684a4e69f5b45f01
fierdy_pandu <lifeisubw@gmail.com>
2020-02-04 17:51:41 +0700

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_123123) do
+ActiveRecord::Schema.define(version: 2020_02_04_102442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
